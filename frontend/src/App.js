45fcbd830ddbc1fc59a5d479454c055258463690
minhduc1912 <luongtranminhduc@gmail.com>
2024-04-23 10:51:22 +0700

    DoAnWeb

---
+++ frontend/src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import SummaryApi from './common';
+import Context from './context';
+import { setUserDetails } from './store/userSlice';
+import { useDispatch } from 'react-redux';
+
+function App() {
+  const dispatch = useDispatch()
+
+  const fetchUserDetails = async()=>{
+      const dataResponse = await fetch(SummaryApi.current_user.url,{
+        method : SummaryApi.current_user.method,
+        credentials : 'include'
+      })
+
+      const dataApi = await dataResponse.json()
+
+      if(dataApi.success){
+        dispatch(setUserDetails(dataApi.data))
+      }
+  }
+
+  
+
+  useEffect(()=>{
+    /**user Details */
+
+
+  },[fetchUserDetails()])
+  return (
+    <>
+      <Context.Provider value={{
+          fetchUserDetails, // user detail fetch 
+      }}>
+        <ToastContainer 
+          position='top-center'
+        />
+        
+        <Header/>
+        <main className='min-h-[calc(100vh-120px)] pt-16'>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </Context.Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import ForgotPassword from '../pages/ForgotPassword'
+import SignUp from '../pages/SignUp'
+import AllProducts from '../pages/AllProducts'
+import AdminPanel from '../pages/AdminPanel'
+import AllUsers from '../pages/AllUsers'
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children : [
+        {
+          path : "" ,
+          element : <Home/>
+        },
+        {
+          path : "/login",
+          element : <Login/>
+        },
+        {
+          path : "/forgot-password",
+          element : <ForgotPassword/>
+        },
+        {
+          path : "/sign-up",
+          element : <SignUp/>
+        },
+        {
+          path : "admin-panel",
+          element : <AdminPanel/>,
+          children : [
+              {
+                  path : "all-users",
+                  element : <AllUsers/>
+              },
+              {
+                  path : "all-products",
+                  element : <AllProducts/>
+              }
+          ]
+      }
+    
+      ]
+    }
+])
+
+export default router
